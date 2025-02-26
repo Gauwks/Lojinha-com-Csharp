bc9f338809edbea966790ef396cf2c3a02acc8d0
Maria Clara <maknae.games7@gmail.com>
2025-02-26 20:51:11 -0300

    Lojinha com C#

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loja_DS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bem-Vindo(a) à nossa lojinha!");
+            Console.WriteLine("Faça seu pedido: ");
+            Console.WriteLine("Jaquetas:");
+            Console.WriteLine("\t-j Jaqueta Addidas Preta - R$199,99");
+            Console.WriteLine("\t-j1 Jaqueta Addidas Azul - R$199,99");
+            Console.WriteLine("\t-j2 Adidas Jaqueta Adicolor Classics SST Originals - R$279,99 \n");
+            Console.WriteLine("Tênis:");
+            Console.WriteLine("\t-a Tênis Nike Air Jordan 5 - R$1709,99");
+            Console.WriteLine("\t-ab Tênis Adidas Breaknet Feminino - Branco - R$189,99");
+            Console.WriteLine("\t-a2 Tênis Fila Player Masculino - Marinho/Vermelho - R$200,49 \n");
+            Console.WriteLine("Camisetas:");
+            Console.WriteLine("\t-a Camiseta Anime Dandadan - R$44,99");
+            Console.WriteLine("\t-s Camisa São Paulo 25/26 - R$349,99");
+            Console.WriteLine("\t-c Camiseta Boxy Com Estampa Charlie Brown Jr - R$79,90");
+
+        }
+    }
+    }
